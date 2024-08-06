3a594b89b2f3167d4e49385719e11a4dbd46daf1
Rahul Naitam <rahulnaitam1992@gmail.comm>
2024-08-06 23:03:32 +0530

    React-Shopping-Cart-App

---
+++ src/component/SearchItem.jsx
@@ -0,0 +1,25 @@
+import React, { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
+import { items } from './Data';
+import Product from './Product';
+
+
+const SearchItem = ({ cart, setCart }) => {
+  const { term } = useParams();
+  const [filterData, setFilterData] = useState([]);
+  useEffect(() => {
+    const filteredData = () => {
+      const data = items.filter((p) => p.title.toLowerCase().includes(term.toLocaleLowerCase()));
+      console.log(data)
+      setFilterData(data)
+    }
+    filteredData();
+  }, [term])
+  return (
+    <div>
+      <Product cart={cart} setCart={setCart} items={filterData} />
+    </div>
+  )
+}
+
+export default SearchItem
